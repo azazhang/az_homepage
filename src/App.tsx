3bcf6b48e989739085ed93b4e43eb6dd23ccec9f
deadtomb <deadtomb@gmail.com>
2025-01-14 23:55:51 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+import styles from "./App.module.css";
 
 function App() {
-  return <div><Message /></div>;
+  const items = [
+    { id: 1, name: "Item 1" },
+    { id: 2, name: "Item 2" },
+    { id: 3, name: "Item 3" },
+  ];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item.id}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
